06ac66bab5a1c1b7b91820bbadd5b0b739b2a211
ThisDavehead <34422661+ThisDavehead@users.noreply.github.com>
2020-12-14 13:30:40 -0700

    Update convert_csv_to_json.py

--- convert_csv_to_json.py
+++ convert_csv_to_json.py
@@ -2,17 +2,30 @@
 @author: David Adams
 """
 
+import sys
 import glob
 import json
-import io
 import csv
 
+# First, a little trick to bypass the maxInt limitations for reading large csv files
+maxInt = sys.maxsize
+
+while True:
+    # decrease the maxInt value by factor 10
+    # as long as the OverflowError occurs.
+
+    try:
+        csv.field_size_limit(maxInt)
+        break
+    except OverflowError:
+        maxInt = int(maxInt/10)
+
 # Path of csv input files.
-input_path = 'PATH_TO_DIRECTORY_CONTAINING_JSONL_FILES_USING_\\_SEPARATORS\\'
+input_path = 'PATH_TO_DIRECTORY_CONTAINING_CSV_FILES_USING_\\_SEPARATORS\\'
 # Make an array of all csv files in the input path.
 files = [f for f in glob.glob(input_path + "**/*.csv", recursive=True)]
 # Path of json output files.
-output_path = 'PATH_TO_TARGET_CSV_DIRECTORY_USING_\\_SEPARATORS\\'
+output_path = 'PATH_TO_TARGET_JSON_DIRECTORY_USING_\\_SEPARATORS\\'
 
 # For each input file:
 #   Make an output filename using the input filename with the extension changed to *.json.
@@ -21,15 +34,15 @@ for f in files:
     name, ext = f.split('.')
     name = name.split('\\')[-1]
     new_file = output_path + '{}.{}'.format(name, 'json')
-    data = {}
-    with open(f, 'r', encoding='utf-8') as csv_input:
-        csvReader = csv.DictReader(csv_input)
-        # Each row should be a dictionary
-        for rows in csvReader:
 
-            # IMPORTANT: Change the following line's rows index to whichever
-            # column acts as a unique identifier for the various rows
-            key = rows['label']
-            data[key] = rows
-    with io.open(new_file, 'w', encoding="utf-8") as json_output_file:
-        json_output_file.write(json.dumps(data, indent=4))
+    # Create list of dictionaries, with each dictionary being a row from the csv file
+    with open(f, 'r', encoding='utf-8') as csv_input:
+        reader = csv.DictReader(csv_input)
+        rows = list(reader)
+
+    # First clear the output file
+    open(new_file).close()
+    with open(new_file, 'w+', encoding='utf-8') as json_output:
+        json.dump(rows, json_output)
+
+print("done!")
